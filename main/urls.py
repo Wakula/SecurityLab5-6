95f18bc57125e50118b34fed51255aee89016af2
Kiril Vakulenko <user4@email.com>
2020-12-11 01:37:10 +0200

    add nginx https support with self-signed certificate

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('storage/', include('storage.urls')),
+    path('', RedirectView.as_view(url='storage/login', permanent=True)),
 ]
